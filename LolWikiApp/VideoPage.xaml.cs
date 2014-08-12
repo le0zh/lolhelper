046595fb634a5696ca927367f6dff19175e37725
le0zh <newlight@qq.com>
2014-08-12 17:43:25 +0800

    video

--- LolWikiApp/VideoPage.xaml.cs
+++ LolWikiApp/VideoPage.xaml.cs
@@ -20,14 +20,30 @@ namespace LolWikiApp
 
         public void PlayVideo()
         {
-            MediaPlayerLauncher player = new MediaPlayerLauncher();
-            player.Media = new Uri("http://115.28.43.55/lol/index.php/YoukuM3u8/getplaylist/type/mp4/youkuid/XNzUzMzgxMDgw");
-            player.Show();
+            //MediaPlayerLauncher player = new MediaPlayerLauncher();
+            //player.Media = new Uri("http://115.28.43.55/lol/index.php/YoukuM3u8/getplaylist/type/mp4/youkuid/XNzUzMzgxMDgw");
+            //player.Show();
+            string html = @"<html>
+<head>
+<meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge' />
+<meta name='viewport' content='width=device-width, initial-scale=1.0, user-scalable=no, minimum-scale=1.0, maximum-scale=1.0'>
+<title>video play</title>
+</head>
+<body>
+<h3>video start</h3>
+<video id='youku-html5-player-video' width='300' height='300' x-webkit-airplay='allow' controls='controls' autoplay='true' src='http://v.youku.com/player/getrealM3U8/vid/XNzUzNDYwNDgw/type/mp4/v.m3u8'></video>
+<h3>video end</h3>
+</body>
+<html>";
+            VideoWebBrowser.NavigateToString(html);
         }
 
-        private void ButtonBase_OnClick(object sender, RoutedEventArgs e)
+        protected override void OnNavigatedTo(NavigationEventArgs e)
         {
             PlayVideo();
+
+            base.OnNavigatedTo(e);
         }
     }
 }
